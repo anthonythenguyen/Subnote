b20b92f2045342d0b2e65bdd28512dd56ca4d520
anthonythenguyen <pantswithlazers@gmail.com>
2021-11-18 15:33:23 -0700

    Adding landing page

---
+++ landing.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Subnote</title>
+</head>
+<body>
+    <p>
+        <?php 
+            print "Henlo world";
+        ?>
+    </p>
+</body>
+</html>
