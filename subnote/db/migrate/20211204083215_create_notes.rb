470dbea0c9798a1591c48fce840ed86a9814756c
Brenden Bunker <brenden.bunker@mx.com>
2021-12-04 11:29:01 -0700

    setup api setup, added note model

---
+++ subnote/db/migrate/20211204083215_create_notes.rb
@@ -0,0 +1,12 @@
+class CreateNotes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :notes do |t|
+      t.string :title
+      t.string :content
+      t.string :ownerid
+      t.string :parentid
+
+      t.timestamps
+    end
+  end
+end
