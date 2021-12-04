e5ab77affcddcb1cda72db0175960a10e9bf2897
Brenden Bunker <brenden.bunker@mx.com>
2021-12-04 11:29:01 -0700

    setup api setup, added note model

---
+++ subnote/app/controllers/api/v1/notes_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::NotesController < ApplicationController
+  def index
+    notes = Note.all
+    render json: notes, status:200
+  end
+
+  def show
+  end
+end
--- subnote/config/routes.rb
+++ subnote/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :notes, only: [:index, :show, :create]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "*path", :to => "home#index"
 
