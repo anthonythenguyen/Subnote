94e83b084ee0e972c418fed996a9b5463d6206d5
Brenden Bunker <brenden.bunker@mx.com>
2021-12-04 13:20:53 -0700

    attached the page nav on login and signup

--- subnote/app/javascript/components/App.js
+++ subnote/app/javascript/components/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    NavLink,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
 } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
@@ -11,57 +11,59 @@ import Signup from "../pages/Signup/Signup";
 import styles from "./App.module.css";
 
 const loginNav = (
-    <nav className={styles.loginSignupContainer}>
-        <NavLink
-            to="/login"
-            className={({ isActive }) =>
-                styles.navButton + (isActive ? " " + styles.activePage : "")
-            }
-        >
-            <p>Login</p>
-        </NavLink>
-        <NavLink
-            to="/signup"
-            className={({ isActive }) =>
-                styles.navButton + (isActive ? " " + styles.activePage : "")
-            }
-        >
-            <p>Sign Up</p>
-        </NavLink>
-    </nav>
+  <nav className={styles.loginSignupContainer}>
+    <NavLink
+      to="/login"
+      className={({ isActive }) =>
+        styles.navButton + (isActive ? " " + styles.activePage : "")
+      }
+    >
+      <p>Login</p>
+      <div className={styles.fillInGap}></div>
+    </NavLink>
+    <NavLink
+      to="/signup"
+      className={({ isActive }) =>
+        styles.navButton + (isActive ? " " + styles.activePage : "")
+      }
+    >
+      <p>Sign Up</p>
+      <div className={styles.fillInGap}></div>
+    </NavLink>
+  </nav>
 );
 function App(props) {
-    return (
-        <Router>
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route
-                    exact
-                    path="/login"
-                    element={
-                        <div className={styles.mainDiv}>
-                            <div className={styles.containerContainer}>
-                                {loginNav}
-                                <Login />
-                            </div>
-                        </div>
-                    }
-                />
-                <Route
-                    exact
-                    path="/signup"
-                    element={
-                        <div className={styles.mainDiv}>
-                            <div className={styles.containerContainer}>
-                                {loginNav}
-                                <Signup />
-                            </div>
-                        </div>
-                    }
-                />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route
+          exact
+          path="/login"
+          element={
+            <div className={styles.mainDiv}>
+              <div className={styles.containerContainer}>
+                {loginNav}
+                <Login />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          exact
+          path="/signup"
+          element={
+            <div className={styles.mainDiv}>
+              <div className={styles.containerContainer}>
+                {loginNav}
+                <Signup />
+              </div>
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 App.propTypes = {};
